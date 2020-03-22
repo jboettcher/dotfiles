b85c17bcdb425ba6edb1876076fcb7bb4ddaa4b6
Jan Böttcher <boettcher@in.tum.de>
2020-03-22 11:43:01 +0100

    added zsh plugins

--- custom.zsh
+++ custom.zsh
@@ -2,3 +2,11 @@ alias vim="nvim"
 export EDITOR="nvim"
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export LANG=en_US.utf-8
+
+plugins=(
+   git
+   docker
+   zsh-syntax-highlighting
+   zsh-autosuggestions
+   vi-mode
+)
