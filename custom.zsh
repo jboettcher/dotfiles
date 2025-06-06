64e7500679c550ee7142f1c50ddc338a65dbd953
Jan Böttcher <jboettcher@salesforce.com>
2025-06-06 12:15:58 +0200

    Minor: Wip

--- custom.zsh
+++ custom.zsh
@@ -17,6 +17,12 @@ case `uname` in
     # commands for OS X go here
        export MAKEFLAGS="-j $(sysctl -n hw.ncpu)"
        export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+       if [ -d "$HOME/bin" ] ; then
+          PATH="$PATH:$HOME/bin"
+       fi
+       alias clangd-18="/opt/homebrew/opt/llvm/bin/clangd"
+       alias fc="falcon login && falcon context import cdp-hyperdb && falcon context apply cdp_hyperdb026_test1_uswest2_cdp001 && falcon credentials request --duration=31d --major-reason=Demo --details=Demo && falcon credentials config --role=PCSKDeveloperRole && falcon kube config && kubectl config set-context --current --namespace=cdp && cd /tmp/byoh/test1-cdp001 && python3 ~/hyper-devtools/connect-to-falcon/fetch_certs.py cdp-control cdp && falcon context apply cdp_hyperdb016_dev1_uswest2_cdp001 && falcon credentials request --duration=31d --major-reason=Demo --details=Demo && falcon credentials config --role=PCSKDeveloperRole && falcon kube config && kubectl config set-context --current --namespace=cdp && cd /tmp/byoh/dev1-cdp001 && python3 ~/hyper-devtools/connect-to-falcon/fetch_certs.py cdp-control cdp && falcon context apply cdp_hyperdb031_dev1_uswest2_cdp002 && falcon credentials request --duration=31d --major-reason=Demo --details=Demo && falcon credentials config --role=PCSKDeveloperRole && falcon kube config && kubectl config set-context --current --namespace=cdp && cd /tmp/byoh/dev1-cdp002 && python3 ~/hyper-devtools/connect-to-falcon/fetch_certs.py cdp-control cdp && falcon context apply cdp_hyperdb099_perf2_uswest2_cdp1 && falcon credentials request --duration=31d --major-reason=Demo --details=Demo && falcon credentials config --role=PCSKDeveloperRole && falcon kube config && kubectl config set-context --current --namespace=cdp && cd /tmp/byoh/perf2-cdp1 && python3 ~/hyper-devtools/connect-to-falcon/fetch_certs.py cdp-control cdp && falcon context apply cdp_hyperdb1_test1_uswest2_cdp002 && falcon credentials request --duration=31d --major-reason=Demo --details=Demo && falcon credentials config --role=PCSKDeveloperRole && falcon kube config && kubectl config set-context --current --namespace=cdp && cd /tmp/byoh/test1-cdp002 && python3 ~/hyper-devtools/connect-to-falcon/fetch_certs.py cdp-control cdp"
+
   ;;
   Linux)
     # commands for Linux go here
